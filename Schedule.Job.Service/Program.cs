076e960c7c72f53248b57529bc2bde0e35f23759
Jason Lakhan <jason.lakhan@sportsengine.com>
2023-05-12 10:27:17 -0400

    Project Setup

---
+++ Schedule.Job.Service/Program.cs
@@ -0,0 +1,11 @@
+﻿using Schedule.Job.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
---
+++ Schedule.Orchestrator/Program.cs
@@ -0,0 +1,11 @@
+﻿using Schedule.Orchestrator;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
